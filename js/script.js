bc1d34882616c49a9fb193c7ce9310fa2487c739
Senmetsu <samguliker@hotmail.com>
2018-05-02 13:20:00 +0200

    not the patch that I want

--- js/script.js
+++ js/script.js
@@ -6,21 +6,23 @@
         const img = document.querySelector('.portfolio-img')
         const button = document.querySelector('.portfolio-item a')
         const project = document.querySelector('.portfolio-item p')
-        const headline = document.querySelector('.portfolio-item h2')
-        const h1 = document.querySelector('.portfolio-item h1')
+        const h2 = document.querySelector('h2')
+        const h3 = document.querySelector('h3')
+
+        console.log('h2')
 
         button.addEventListener('mouseover', function(){
           img.classList.add('layer-3')
-          headline.classList.add('layer-1')
+          h2.classList.add('layer-1')
           project.classList.add('layer-2')
-          h1.classList.add('layer-4')
+          h3.classList.add('layer-4')
         })
 
         button.addEventListener('mouseout', function(){
           img.classList.remove('layer-3')
-          headline.classList.remove('layer-1')
+          h2.classList.remove('layer-1')
           project.classList.remove('layer-2')
-          h1.classList.remove('layer-4')
+          h3.classList.remove('layer-4')
         })
 
       }
