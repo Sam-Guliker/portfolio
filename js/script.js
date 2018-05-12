7bc5fb1fa94243fda41d832185e5140df5857820
Senmetsu <samguliker@hotmail.com>
2018-05-12 21:36:32 +0200

    update of responsiveness

--- js/script.js
+++ js/script.js
@@ -3,27 +3,30 @@
   var app = {
 
       init: function(){
-        const img = document.querySelector('.portfolio-img')
-        const button = document.querySelector('.portfolio-item a')
-        const project = document.querySelector('.portfolio-item p')
-        const h2 = document.querySelector('h2')
-        const h3 = document.querySelector('h3')
+        const img = document.querySelectorAll('.portfolio-img')
+        const button = document.querySelectorAll('.portfolio-item a')
+        const project = document.querySelectorAll('.portfolio-item p')
+        const h2 = document.querySelectorAll('h2')
+        const h3 = document.querySelectorAll('h3')
 
-        console.log('h2')
+        for (var i = 0; i < button.length; i++) {
+            button[i].addEventListener('mouseover', function(){
+              console.log(img)
+              img.classList.add('layer-3')
+              h2.classList.add('layer-1')
+              project.classList.add('layer-2')
+              h3.classList.add('layer-4')
+            })
 
-        button.addEventListener('mouseover', function(){
-          img.classList.add('layer-3')
-          h2.classList.add('layer-1')
-          project.classList.add('layer-2')
-          h3.classList.add('layer-4')
-        })
+            button[i].addEventListener('mouseout', function(){
+              img.classList.remove('layer-3')
+              h2.classList.remove('layer-1')
+              project.classList.remove('layer-2')
+              h3.classList.remove('layer-4')
+            })
+        }
 
-        button.addEventListener('mouseout', function(){
-          img.classList.remove('layer-3')
-          h2.classList.remove('layer-1')
-          project.classList.remove('layer-2')
-          h3.classList.remove('layer-4')
-        })
+
 
       }
     }
