1886efc0baac6c53899a9d4c73cc6d5428911076
Sam-Guliker <samguliker@hotmail.com>
2018-06-09 15:55:23 +0200

    added js stuff

--- js/script.js
+++ js/script.js
@@ -1,33 +1,42 @@
-(function (){
-
+(function() {
   var app = {
+    animation: function() {
+      const img = document.querySelector(".portfolio-img");
+      const button = document.querySelector(".portfolio-item a");
+      const project = document.querySelector(".portfolio-item p");
+      const h2 = document.querySelector(".portfolio-item h2");
+      const h3 = document.querySelector("h3");
+      console.log(h2);
 
-      init: function(){
-        const img = document.querySelector('.portfolio-img')
-        const button = document.querySelector('.portfolio-item a')
-        const project = document.querySelector('.portfolio-item p')
-        const h2 = document.querySelector('.portfolio-item h2')
-        const h3 = document.querySelector('h3')
-        console.log(h2)
+      button.addEventListener("mouseover", function() {
+        img.classList.add("layer-3");
+        h2.classList.add("layer-1");
+        project.classList.add("layer-2");
+        h3.classList.add("layer-4");
+      });
 
+      button.addEventListener("mouseout", function() {
+        img.classList.remove("layer-3");
+        h2.classList.remove("layer-1");
+        project.classList.remove("layer-2");
+        h3.classList.remove("layer-4");
+      });
+    },
 
-        button.addEventListener('mouseover', function(){
-          img.classList.add('layer-3')
-          h2.classList.add('layer-1')
-          project.classList.add('layer-2')
-          h3.classList.add('layer-4')
-        })
+    footerHandler: function() {
+      const email = document.querySelector(".email");
+      const github = document.querySelector(".github");
 
-        button.addEventListener('mouseout', function(){
-          img.classList.remove('layer-3')
-          h2.classList.remove('layer-1')
-          project.classList.remove('layer-2')
-          h3.classList.remove('layer-4')
-        })
+      email.addEventListener("click", function() {
+        window.open("mailto:samguliker@hotmail.com");
+      });
 
-      }
+      github.addEventListener("click", function() {
+        window.open("https://github.com/Sam-Guliker/", "_blank");
+      });
     }
+  };
 
-  app.init()
-
+  app.animation();
+  app.footerHandler();
 })();
